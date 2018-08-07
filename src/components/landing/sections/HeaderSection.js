dd998428059b43df95182c15a12f6a0c7e8bdc35
Keith McCall <keithmccall@Keiths-Macbook-Pro-3.local>
2018-08-07 00:31:57 -0400

    update

--- src/components/landing/sections/HeaderSection.js
+++ src/components/landing/sections/HeaderSection.js
@@ -4,9 +4,9 @@ import SVGC from "../../tools/images/SVG";
 const HeaderSection = props => {
   const { content } = props;
   return (
-    <div className="vh flex-column Big-section" id="home">
+    <div className="vh flex-column Big-section relative" id="home">
       <div
-        className="flex-1 Header-section relative"
+        className="flex-1 Header-section"
         style={{ backgroundImage: `url(${content.background})` }}
       >
         <SVGC path={content.logo} className="image centered" fill="white" />
